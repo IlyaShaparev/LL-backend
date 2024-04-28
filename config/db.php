4007289cb7076d94095d004a01b3b34dce44e330
Ilya Shaparev <bis12012003@yandex.ru>
2024-04-28 15:41:07 +0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=learn_language',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
